3cad82da176222b6e69600798cbcfab808e6bbef
AishaLawal1 <aolawal@uark.edu>
2023-03-31 16:16:35 -0500

    updated slurm script

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -1,12 +1,34 @@
 #! /usr/bin/env python3
-jobname="#SBATCH --job-name=pinnacle_slurm"
-queue="#SBATCH --partition comp01"
-number_of_nodes="#SBATCH --nodes=1"
-number_of_processors="#SBATCH --tasks-per-node=1"
-walltime="#SBATCH --time=0:02:00"
 
-print(jobname)
-print(queue)
-print(number_of_nodes)
-print(number_of_processors)
-print(walltime)
+# set your variables
+
+job_name = '<Job-Name>'
+queue = 'comp01'
+walltime = 1
+num_nodes = 1
+num_processors = 24
+
+#print bash header
+print('#!/bin/bash')
+
+print()
+
+#print SBATCH commands
+
+print('#SBATCH --job-name=' + job_name)
+print('#SBATCH --partition',queue)
+print('#SBATCH --nodes='+ str (num_nodes))
+print('#SBATCH --qos comp')
+print('#SBATCH --tasks-per-node=32')
+print('#SBATCH --time=' + str(walltime) + ':00:00')
+print('#SBATCH -o aolawal_%j.out')
+print('#SBATCH -e aolawal_%j.err')
+print('#SBATCH --mail-type=ALL')
+print('#SBATCH --mail-user=aolawal@uark.edu')
+
+print()
+
+#purge all modules
+
+print('purge modules') 
+
