85868c287c5d6094e867512453496d91ec397ca6
AishaLawal1 <aolawal@uark.edu>
2023-04-10 09:45:06 -0500

    modified reading-file script

--- parseGFF.py
+++ parseGFF.py
@@ -6,18 +6,19 @@ import argparse
 
 #create an ArgumentParser object (like a box that holds an argument)
 
-parser = argparse.ArgumentParser(description="This script opens and reads a GFF* file)
+parser = argparse.ArgumentParser(description= "This script opens and reads a GFF file")
 
 #add positional (required) arguments
 
-parser.add_argument("file_name",help="enter the file name", type=str)
-parser.add_argument("FASTA_file",help="enter the FASTA file", type=str)
+parser.add_argument("gff",help="enter the gff", type=str)
+parser.add_argument("fasta",help="enter the FASTA file", type=str)
 
 args = parser.parse_args()
 
- file = covid.gff
- 
-with open(file_name) as f:
-    for line in f:
-        print(line)
+#open the gff file
 
+with open(args.gff) as x:
+#loop over all the lines in the file
+	for line in x:
+		print(line)
+# To run the data, we can make a link to it by using the ln command line in linux
